33aaf524e35023a9993409b4d38611f8b935992c
tsvetomira dimitrova <tsvetomira.dimitrova@docuware.com>
2020-03-29 17:01:34 +0300

    Modern School  by TD

---
+++ ModernSchool/LessonTime.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernSchool
+{
+    class LessonTime
+    {
+        public DateTime startingHour;
+        public DateTime endHour;
+
+        public LessonTime(int startingHour, int endHour)
+        {
+            this.startingHour = new DateTime(DateTime.Now.Year,
+                DateTime.Now.Month,
+                DateTime.Now.Day,
+                startingHour, 0, 0);
+            this.endHour = new DateTime(DateTime.Now.Year,
+                DateTime.Now.Month,
+                DateTime.Now.Day,
+                endHour, 0, 0);
+        }
+
+        public bool Compare(LessonTime lessonTime)
+        {
+            bool result = (lessonTime.startingHour >= this.startingHour &&
+                lessonTime.startingHour < this.endHour) ||
+                (lessonTime.endHour > this.startingHour &&
+                lessonTime.endHour <= this.endHour);
+
+            return result;
+        }
+
+        public static int CompareLessonTimes(LessonTime l1, LessonTime l2)
+        {
+            if (l1.startingHour < l2.startingHour)
+            {
+                return -1;
+            }
+            else if (l1.startingHour > l2.startingHour)
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+
+}
---
+++ ModernSchool/Student.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernSchool
+{
+    class Student
+    {
+        private List<LessonTime> busyHours = new List<LessonTime>();
+        private List<string> lessonsAttended = new List<string>();
+        private string name; 
+
+        public Student(string name, LessonTime lesson1, LessonTime lesson2)
+        {
+            this.busyHours.Add(lesson1);
+            this.busyHours.Add(lesson2);
+            this.name = name;
+        }
+
+        private bool isFree(LessonTime lessonTime)
+        {
+            foreach (var hour in this.busyHours)
+            {
+                if (hour.Compare(lessonTime))
+                {
+                    return false;
+                }
+                //return !hour.Compare(lessonTime);    !!!WRONG!!!          
+            }
+            return true;
+        }
+
+        public void Learn(string lesson, LessonTime lessonTime)
+        {
+            if(isFree(lessonTime))
+            {
+                lessonsAttended.Add(lesson);
+                busyHours.Add(lessonTime);
+            }
+        }
+        public void PrintStudentResult()
+        {
+            Console.WriteLine($"The student {name} participates in lectures: ");
+            foreach (var lesson in lessonsAttended)
+            {
+                Console.WriteLine(lesson);
+            }
+
+            busyHours.Sort(LessonTime.CompareLessonTimes);
+
+            foreach (var time in busyHours)
+            {
+                Console.WriteLine($" and he is busy between {time.startingHour} and {time.endHour}");
+            }
+        }
+
+        
+    }
+}
