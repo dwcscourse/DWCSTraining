dc25af027a1460b8e7e541cf17fb9aa554786283
Ana Marinova <ana.marinovva@gmail.com>
2020-04-02 00:50:24 +0300

    Exercise 02.04

---
+++ Casino/Game.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino
+{
+    class Game
+    {
+        private int rounds;
+        private List<Player> players;
+        public Game(int rounds, List<Player> players)
+        {
+            this.rounds = rounds;
+            this.players = players;
+        }
+
+        public void PlayRound()
+        {
+            int max=0;
+            string winner;
+            bool isDuplicate = true;
+            while (isDuplicate == true)
+            {
+                foreach (Player player in players)
+                {
+                    int dice = player.ThrowDice();
+                    if (dice > max)
+                    {
+                        max = dice;
+                    }
+                    else if (dice == max)
+                    {
+                        isDuplicate = false;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Casino/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino
+{
+    class Player
+    {
+        private Random rnd = new Random();
+        private string name;
+        public Player(string name)
+        {
+            this.name = name;
+        }
+        public string Name 
+        {
+            get 
+            {
+                return this.name;
+            }
+        }
+        
+        public int ThrowDice()
+        {
+            return rnd.Next(1, 7);            
+        }
+    }
+}
