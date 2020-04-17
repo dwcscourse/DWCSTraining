8bfd90e443f46ce59d0d4f83838d260000435117
tsvetomira dimitrova <tsvetomira.dimitrova@docuware.com>
2020-04-17 17:20:17 +0300

    IntroduceYourself  test

--- IntroduceYourself/Program.cs
+++ IntroduceYourself/Program.cs
@@ -11,12 +11,14 @@ namespace IntroduceYourself
         static void Main(string[] args)
         {
 
-            //Person Bela = new Person("Bella", "Borisova", 6);
-            //Person Ani = new Student("Ana", "Marinova", 22, "TU", "KSI");
-            //Student Tsveti = new Student("Tsvety", "Dimitrova", 36, "SU", "Math");
+            Person Bela = new Person("Bella", "Borisova", 6);
+            Person Ani = new Student("Ana", "Marinova", 22, "TU", "KSI");
+            Student Tsveti = new Student("Tsvety", "Dimitrova", 36, "SU", "Math");
 
-            //Ani.IntroduceYourself();
-            //Bela.IntroduceYourself();
+            Bela.IntroduceYourself();
+            Ani.IntroduceYourself();
+            Tsveti.IntroduceYourself();
+            Tsveti.SaySomething();
 
             List<Person> people = new List<Person>()
             {
