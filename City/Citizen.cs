729246401244e1ac7ca8257acc5293ae94382b50
tsvetomira dimitrova <tsvetomira.dimitrova@docuware.com>
2020-04-20 17:25:07 +0300

    Homework City TD

---
+++ City/Citizen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace City
+{
+    public class Citizen : Habitant
+    {
+        private string name;
+        private int age;
+
+        public Citizen(string name, int age, string ID) : base (ID)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
+}
