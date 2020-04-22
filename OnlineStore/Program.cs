f99021207fa593ff3462e35d06ddfe53e6a44dda
Ana Marinova <ana.marinovva@gmail.com>
2020-04-22 10:07:51 +0300

    test

--- OnlineStore/Program.cs
+++ OnlineStore/Program.cs
@@ -11,13 +11,8 @@ namespace OnlineStore
         static void Main(string[] args)
         {
             string client;
-            int choice = 0;
             ClientType type = ClientType.regular;
-            bool shouldContinue = true;
-            RegularShoppingCart regCart = new RegularShoppingCart();
-            PremiumShoppingCart premCart = new PremiumShoppingCart();
-            PlatinumShoppingCart platCart = new PlatinumShoppingCart();
-            
+
             List<Client> clients = new List<Client>()
             {
                 new Client("Bella", ClientType.platinum),
@@ -38,106 +33,42 @@ namespace OnlineStore
             client = Console.ReadLine();
             foreach (Client item in clients)
             {
-                if(client == item.Name)
+                if (client == item.Name)
                 {
                     type = item.Type;
                 }
             }
+
+            ShoppingCart cart = GetCart(type);
+
             Console.WriteLine("List: \n 1.Bread \n 2.Rice \n 3.Toilet Paper \n 4.Water \n 5.Wine \n 6.Stop shopping");
-            do
-            {
-                choice = int.Parse(Console.ReadLine());
-                switch (choice)
-                {
-                    case 1:
-                        if (type == ClientType.regular)
-                        {
-                            regCart.AddItem(items[0]);
-                        }
-                        else if (type == ClientType.premium)
-                        {
-                            premCart.AddItem(items[0]);
-                        }
-                        else if (type == ClientType.platinum)
-                        {
-                            platCart.AddItem(items[0]);
-                        }
-                        break;
-                    case 2:
-                        if (type == ClientType.regular)
-                        {
-                            regCart.AddItem(items[1]);
-                        }
-                        else if (type == ClientType.premium)
-                        {
-                            premCart.AddItem(items[1]);
-                        }
-                        else if (type == ClientType.platinum)
-                        {
-                            platCart.AddItem(items[1]);
-                        }
-                        break;
-                    case 3:
-                        if (type == ClientType.regular)
-                        {
-                            regCart.AddItem(items[2]);
-                        }
-                        else if (type == ClientType.premium)
-                        {
-                            premCart.AddItem(items[2]);
-                        }
-                        else if (type == ClientType.platinum)
-                        {
-                            platCart.AddItem(items[2]);
-                        }
-                        break;
-                    case 4:
-                        if (type == ClientType.regular)
-                        {
-                            regCart.AddItem(items[3]);
-                        }
-                        else if (type == ClientType.premium)
-                        {
-                            premCart.AddItem(items[3]);
-                        }
-                        else if (type == ClientType.platinum)
-                        {
-                            platCart.AddItem(items[3]);
-                        }
-                        break;
-                    case 5:
-                        if (type == ClientType.regular)
-                        {
-                            regCart.AddItem(items[4]);
-                        }
-                        else if (type == ClientType.premium)
-                        {
-                            premCart.AddItem(items[4]);
-                        }
-                        else if (type == ClientType.platinum)
-                        {
-                            platCart.AddItem(items[4]);
-                        }
-                        break;
-                    case 6:
-                        shouldContinue = false;
-                        break;
-                }
-            } while (shouldContinue);
 
-            if (type == ClientType.regular)
+            string[] inputItems = Console.ReadLine().Split(',');
+
+            foreach (string item in inputItems)
             {
-                Console.WriteLine(regCart.CalculateBill());
-            }
-            else if (type == ClientType.premium)
-            {
-                Console.WriteLine(premCart.CalculateBill());  
-            }
-            else if (type == ClientType.platinum)
-            {
-                Console.WriteLine(platCart.CalculateBill());
+                int number = int.Parse(item);
+                cart.AddItem(items[number - 1]);
             }
 
+            Console.WriteLine(cart.CalculateBill());
+        }
+
+        private static ShoppingCart GetCart(ClientType clientType)
+        {
+            if (clientType == ClientType.regular)
+            {
+                return new RegularShoppingCart();
+            }
+            else if (clientType == ClientType.premium)
+            {
+                return new PremiumShoppingCart();
+            }
+            else if (clientType == ClientType.platinum)
+            {
+                return new PlatinumShoppingCart();
+            }
+            return new RegularShoppingCart();
         }
     }
 }
